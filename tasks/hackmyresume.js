f64b7ed13440359d9375b4e4538d85527eba413d
Murray Christopherson <murraychristopherson@gmail.com>
2017-10-03 23:08:23 -0400

    Fixing the JSHint errors

--- tasks/hackmyresume.js
+++ tasks/hackmyresume.js
@@ -22,12 +22,50 @@ module.exports = function(grunt) {
   var HMRERR = require( 'hackmyresume/dist/cli/error' );
   var _out;
 
-  // Register the task
-  grunt.registerMultiTask(
-    'hackmyresume',
-    'Grunt plugin for HackMyResume.',
-    _task
-  );
+
+
+/** Generate resumes through the HackMyResume API. */
+  function _build( srcArray, destFile, options ) {
+
+    try {
+      grunt.log.writeln( 'Launching HackMyResume...' );
+
+      // Going through the low-level HMR interface. Set up a verb
+      var v = new HMR.verbs.build();
+
+      // Add error / output handlers so HMR colored output displays in Grunt.
+      v.on('hmr:error', function(ex) {
+        HMRERR.err.apply(HMRERR, arguments);
+        grunt.warn('An error occurred during HackMyResume resume generation.');
+      });
+      v.on('hmr:status', function() {
+        _out.do.apply(_out, arguments);
+      });
+      options.errHandler = v;
+
+      // Invoke the verb and kick off resume generation
+      v.invoke.call( v, srcArray, [destFile], options, function() { } );
+      if( v.errorCode ) {
+        grunt.warn('HackMyResume exited with error ' + v.errorCode + '.');
+      }
+
+      // TODO: Use grunt.file.write|read against the string version of the
+      // HackMyResume API.
+    }
+    catch( ex ) {
+
+      var msg = ex.toString();
+      if( ex.stack ) {
+        msg += ex.stack;
+      }
+      grunt.warn( msg );
+
+      return;
+    }
+
+    // Print a success message.
+    grunt.log.writeln('Resume(s) successfully generated to "' + destFile + '".');
+  }
 
 
 
@@ -65,47 +103,12 @@ module.exports = function(grunt) {
 
 
 
-  /** Generate resumes through the HackMyResume API. */
-  function _build( srcArray, destFile, options ) {
-
-    try {
-      grunt.log.writeln( 'Launching HackMyResume...' );
-
-      // Going through the low-level HMR interface. Set up a verb
-      var v = new HMR.verbs.build();
-
-      // Add error / output handlers so HMR colored output displays in Grunt.
-      v.on('hmr:error', function(ex) {
-        HMRERR.err.apply(HMRERR, arguments);
-        grunt.warn('An error occurred during HackMyResume resume generation.');
-      });
-      v.on('hmr:status', function() {
-        _out.do.apply(_out, arguments);
-      });
-      options.errHandler = v;
-
-      // Invoke the verb and kick off resume generation
-      v.invoke.call( v, srcArray, [destFile], options, function() { } );
-      if( v.errorCode ) {
-        grunt.warn('HackMyResume exited with error ' + v.errorCode + '.');
-      }
-
-      // TODO: Use grunt.file.write|read against the string version of the
-      // HackMyResume API.
-    }
-    catch( ex ) {
-
-      var msg = ex.toString();
-      if( ex.stack )
-        msg += ex.stack;
-      grunt.warn( msg );
-
-      return;
-    }
-
-    // Print a success message.
-    grunt.log.writeln('Resume(s) successfully generated to "' + destFile + '".');
-  }
+  // Register the task
+  grunt.registerMultiTask(
+    'hackmyresume',
+    'Grunt plugin for HackMyResume.',
+    _task
+  );
 
 
 
